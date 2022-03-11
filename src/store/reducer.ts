16470aa80d54b39f5a970da1211158dffd56062b
legocactus <0xcactus@solend.fi>
2022-03-11 12:03:52 -0800

    rotate figment key

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -55,7 +55,7 @@ export const networks: Networks = {
   mainnet: {
     // Cluster.
     label: "Mainnet Beta",
-    url: "https://solana--mainnet.datahub.figment.io/apikey/0e1a11d5b5daf922a4016206301cd913",
+    url: "https://solana--mainnet.datahub.figment.io/apikey/59741b3c5f485924dcf95d6b7549eeed",
     explorerClusterSuffix: "",
     multisigProgramId: new PublicKey(
       "BLg8mSPvEjzSkbGdE9mRJfTSm7EauYgzFsWhERKn1gRm"
