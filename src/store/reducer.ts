ae6229b26b7e9168228b1e98dc17c07cfbc3610f
nope <83512286+nope-finance@users.noreply.github.com>
2022-09-01 17:44:08 -0700

    change rpc again

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -55,7 +55,7 @@ export const networks: Networks = {
   mainnet: {
     // Cluster.
     label: "Mainnet Beta",
-    url: "https://solana-mainnet.g.alchemy.com/v2/ZT3c4pYf1inIrB0GVDNR7nx4LwyED5Ci",
+    url: "https://solendf-solendf-67c7.rpcpool.com/",
     explorerClusterSuffix: "",
     multisigProgramId: new PublicKey(
       "BLg8mSPvEjzSkbGdE9mRJfTSm7EauYgzFsWhERKn1gRm"
